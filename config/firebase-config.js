73759731b04f00705a735519e8dc2c6ed02ffdc4
greentorrance1-art <greentorrance1@gmail.com>
2026-04-14 17:30:36 -0400

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,12 +1,1 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBJjGT5Zp7tV19KHUu4T2Y79v1xHL5ScCs",
-  authDomain: "gmc-site-f2ad6.firebaseapp.com",
-  projectId: "gmc-site-f2ad6",
-  storageBucket: "gmc-site-f2ad6.firebasestorage.app",
-  messagingSenderId: "533541938506",
-  appId: "1:533541938506:web:7925d1472e646db2e7cdb3"
-};
 
-if (typeof firebase !== 'undefined' && !firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
