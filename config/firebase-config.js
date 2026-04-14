656c026c19a55d85e4c029d9eed67ca0941c32cd
greentorrance1-art <greentorrance1@gmail.com>
2026-04-14 14:48:53 -0400

    Create firebase-config.js

---
+++ config/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJjGT5Zp7tV19KHUu4T2Y79v1xHL5ScCs",
+  authDomain: "gmc-site-f2ad6.firebaseapp.com",
+  projectId: "gmc-site-f2ad6",
+  storageBucket: "gmc-site-f2ad6.firebasestorage.app",
+  messagingSenderId: "533541938506",
+  appId: "1:533541938506:web:7925d1472e646db2e7cdb3"
+};
